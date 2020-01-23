d9bd9345fa9a1916ba471987253124ba3364f2bf
Mikie4 <44328779+Mikie4@users.noreply.github.com>
2020-01-23 08:51:02 -0500

    Added Files via Upload

---
+++ ADInactiveComputerCleanup.ps1
@@ -0,0 +1,43 @@
+﻿<#
+    Discovers and Disables AD Computer Objects that have not
+    logged in in provided timeframe. Then moves disabled objects
+    to the 'Disable and Delete' OU.
+#>
+
+$disableAndDeleteDN = "OU=Disable and Delete,DC=pfbiwv,DC=com"
+
+<#
+    Get list of AD Computers that aren't already in 'Disable
+    and Delete' OU based on lastLogonDate.
+#>
+
+$ADComputers = Get-ADComputer -Filter * -Properties * | `
+   ? lastLogonDate -lt ((Get-Date).AddDays(-21)) | `
+   ? DistinguishedName -notlike "*Disable and Delete*" | `
+   Select SamAccountName,LastLogonDate,DistinguishedName
+
+ForEach($ADComputer in ($ADComputers | Sort SamAccountName)){
+    $ADComputerName=$ADComputer.SamAccountName
+    $CanPing=Test-Connection $ADComputerName -Count 1 -Quiet
+    $ADComputerDN=$ADComputer.DistinguishedName
+    If(-not $CanPing){
+        Disable-ADAccount $ADComputerName
+        Sleep 5
+        If((Get-ADComputer $ADComputerName | Select -ExpandProperty Enabled)){
+            $Status = "Disable Failed"
+        }
+        else{
+            Move-ADObject $ADComputerDN $disableAndDeleteDN
+            If((Get-ADComputer $ADComputerName | Select -ExpandProperty DistinguishedName).Split(',')[1] -like "*OU=DISABLE and Delete*"){
+                $Status = "Move Successful"
+            }
+            Else{
+                $Status = "Move Failed"
+            }
+        }
+
+    }
+    Write-Host "$ADComputerName `t $CanPing `t $Status `t"
+    Write-HOst "$ADComputerDN"
+    Write-Host ""
+}
